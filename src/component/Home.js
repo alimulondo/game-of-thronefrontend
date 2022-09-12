0f1c79b298bf63a896ea131a92ae3eb3c4992246
Ali Mulondo <myalimul@gmail.com>
2022-09-12 15:24:59 +0100

    Add dummy data to the house component.

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,12 +2,23 @@ import React, { useEffect } from "react"
 import NavHead from "./NavHead"
 import House from "./House"
 
+const houses =[
+        {"name":"House Ambrose", "region":"The Reach","words": "Never Resting", "id":4},
+        {"name":"House Arryn of Gulltown", "region":"The North","words": "The Vale", "id":6},
+        {"name":"House Ashwood", "region":"The North","words": "", "id":9},
+        {"name":"House Ashwood", "region":"The North","words": "", "id":10},
+
+]
+
 function Home() {
   return (
     <>
         <NavHead />
+        {houses.map((house)=>{
+           return <House name = {house.name} region = {house.region} words = {house.words} id = {house.id}/>;
+        })}
 
-        <House />
+        
     </>
   )
 }
--- src/component/House.js
+++ src/component/House.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react"
 
-function House() {
+
+
+function House(props) {
   return (
     <>
       
  <div className="card w-100 mt-5">
   <div className="card-body">
-    <h5 className="card-title">House Name</h5>
-    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 className="card-title">{props.name}</h5>
+    <p className="card-text">Region : {props.region}</p>
+    <p className="card-text">Words : {props.words}</p>
     <a href="#" className="btn btn-primary">Detials</a>
   </div>
 </div>
